186a75e493c00c18e008fcffa939951715135dc7
Shaye Mine <shayemine775@gmail.com>
2023-12-14 18:49:48 +0100

    modified the requirements.txt

--- init.py
+++ init.py
@@ -8,7 +8,7 @@ db = SQLAlchemy()
 
 def create_app():
     app=Flask(__name__)
-    app.config["SECRET_KEY"] = "12345mine"
+    app.config["SECRET_KEY"] = "secret-key"
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
     db.init_app(app)
 
